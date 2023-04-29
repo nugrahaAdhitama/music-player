2d087d9c7b50d682fb3fa9139f40083da3315ed7
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-04-29 16:43:28 +0700

    Add play, pause, and stop controls using song data from JSON
    In this commit, we have added the play, pause, and stop controls for the Music Player application, utilizing the song data from the song.json file. The controls are now functional and allow the user to interact with the music player.
    
    We have modified the displaySongList function to store the fetched song data into the 'songsData' variable and added click event listeners to the song items in the list. This enables the user to select a song from the list to play.
    
    The playSong function has been updated to play the selected song based on the 'currentSongIndex' and 'songsData' variables. The song data, including the audio file path, is now fetched from the JSON file, providing a more dynamic and maintainable approach to handling song data.
    
    This commit improves the user experience and interaction with the music player controls.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 const songList = document.querySelector(".song-list");
+const playBtn = document.querySelector(".play-btn");
+const pauseBtn = document.querySelector(".pause-btn");
+const stopBtn = document.querySelector(".stop-btn");
+
+playBtn.addEventListener("click", playSong);
+pauseBtn.addEventListener("click", pauseSong);
+stopBtn.addEventListener("click", stopSong);
+
+let currentSongIndex = 0;
+let songsData = [];
 
 async function fetchSongData() {
     const response = await fetch("assets/song.json");
@@ -8,13 +18,41 @@ async function fetchSongData() {
 
 async function displaySongList() {
     const songs = await fetchSongData();
+    songsData = songs;
 
     songs.forEach(song => {
         const songItem = document.createElement("li");
         songItem.className = "song-item";
         songItem.textContent = `${song.title} - ${song.artist}`;
+        songItem.addEventListener("click", () => {
+            currentSongIndex = index;
+            playSong();
+        })
         songList.appendChild(songItem);
     })
+}
+
+function playSong() {
+    if (audio) {
+        audio.pause();
+    }
+
+    const song = songsData[currentSongIndex];
+    audio = new Audio(`assets/music/${song.filename}`);
+    audio.play();
+}
+
+function pauseSong() {
+    if (audio) {
+        audio.pause();
+    }
+}
+
+function stopSong() {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
 }
 
 displaySongList();
