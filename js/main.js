1199c121c9aed2087aaa516b4e2d92e8abd71f28
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-04-29 17:16:38 +0700

    Add song list display toggle and improve initial page load appearance
    This commit introduces several improvements to the music player, including the ability to toggle the song list display and a better initial page load appearance. The following changes have been made:
    
    1. Modified index.html, styles.css, and main.js to hide the album cover, song title, and artist name on initial page load, ensuring that only the song list is displayed at first.
    
    2. Added a "Tampilkan Daftar Lagu" button in the index.html file, under the song-info div.
    
    3. In the main.js file, added an event listener for the new button to handle clicks and call the showSongList function.
    
    4. Created a new showSongList function in main.js that displays the song list and hides the album cover, song title, and artist name.
    
    5. Updated the song item click event listener in main.js to show the album cover, song title, and artist name while hiding the song list when a song is selected.
    
    With these changes, users can easily navigate between the song list and the currently playing song, and the initial page load appearance is more user-friendly.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,21 @@ const songList = document.querySelector(".song-list");
 const playBtn = document.querySelector(".play-btn");
 const pauseBtn = document.querySelector(".pause-btn");
 const stopBtn = document.querySelector(".stop-btn");
+const albumCover = document.getElementById("album-cover");
+const coverImage = document.getElementById("cover-image");
+const songTitle = document.getElementById("song-title");
+const artistName = document.getElementById("artist-name");
+const songListContainer = document.getElementById("song-list-container");
+const showSongListBtn = document.getElementById("show-song-list-btn");
 
 playBtn.addEventListener("click", playSong);
 pauseBtn.addEventListener("click", pauseSong);
 stopBtn.addEventListener("click", stopSong);
+showSongListBtn.addEventListener("click", showSongList);
 
 let currentSongIndex = 0;
 let songsData = [];
+let audio = null;
 
 async function fetchSongData() {
     const response = await fetch("assets/song.json");
@@ -20,16 +28,22 @@ async function displaySongList() {
     const songs = await fetchSongData();
     songsData = songs;
 
-    songs.forEach(song => {
+    songs.forEach((song, index) => {
         const songItem = document.createElement("li");
         songItem.className = "song-item";
         songItem.textContent = `${song.title} - ${song.artist}`;
         songItem.addEventListener("click", () => {
             currentSongIndex = index;
             playSong();
-        })
+
+            coverImage.classList.remove("hidden");
+            songTitle.classList.remove("hidden");
+            artistName.classList.remove("hidden");
+            songListContainer.classList.add("hidden");
+            showSongListBtn.classList.remove("hidden");
+        });
         songList.appendChild(songItem);
-    })
+    });
 }
 
 function playSong() {
@@ -40,6 +54,8 @@ function playSong() {
     const song = songsData[currentSongIndex];
     audio = new Audio(`assets/music/${song.filename}`);
     audio.play();
+
+    updateSongInfo();
 }
 
 function pauseSong() {
@@ -53,6 +69,19 @@ function stopSong() {
         audio.pause();
         audio.currentTime = 0;
     }
+}
+
+function updateSongInfo() {
+    const song = songsData[currentSongIndex];
+    coverImage.src = song.cover;
+    songTitle.textContent = song.title;
+    artistName.textContent = song.artist;
+}
+
+function showSongList() {
+    coverImage.classList.add("hidden");
+    songListContainer.classList.remove("hidden");
+    showSongListBtn.classList.add("hidden");
 }
 
 displaySongList();
